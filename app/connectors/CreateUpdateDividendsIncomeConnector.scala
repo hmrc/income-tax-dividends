2a4eaf13f07b0d9ee2fac1296f5a5f60e47980f2
ghousedev <49005870+ghousedev@users.noreply.github.com>
2023-02-06 15:14:43 +0000

    SASS-4313: Additional Info (Stock Dividends) - 1608 Create Update Dividends Income Data API

---
+++ app/connectors/CreateUpdateDividendsIncomeConnector.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import com.typesafe.config.ConfigFactory
+import config.AppConfig
+import connectors.httpParsers.CreateUpdateDividendsIncomeHttpParser.{CreateUpdateDividendsIncomeHttpReads, CreateUpdateDividendsIncomeResponse}
+import models.DividendsIncomeDataModel
+import uk.gov.hmrc.http.HeaderCarrier.Config
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
+import utils.TaxYearUtils.convertStringTaxYear
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class CreateUpdateDividendsIncomeConnector @Inject()(http: HttpClient, val appConfig: AppConfig)(implicit ec: ExecutionContext) extends IFConnector {
+
+  override val headerCarrierConfig: Config = HeaderCarrier.Config.fromConfig(ConfigFactory.load())
+
+  def createUpdateDividends(nino: String, taxYear: Int, createUpdatedDividendsIncomeModel: models.DividendsIncomeDataModel)
+                           (implicit hc: HeaderCarrier): Future[CreateUpdateDividendsIncomeResponse] = {
+    val putDividendsIncome = "1608"
+    val taxYearParameter = convertStringTaxYear(taxYear)
+    val url = appConfig.ifBaseUrl + s"/income-tax/income/dividends/$nino/$taxYearParameter"
+
+    http.POST[models.DividendsIncomeDataModel, CreateUpdateDividendsIncomeResponse](url, createUpdatedDividendsIncomeModel)(
+      DividendsIncomeDataModel.formats.writes, CreateUpdateDividendsIncomeHttpReads, ifHeaderCarrier(url, putDividendsIncome), ec)
+  }
+}
